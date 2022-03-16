8a260238eefd80c17c69f7367496e745814f8d95
YaminiSharma <sharmayamini0002@gmail.com>
2022-03-16 12:30:13 +0530

    Created Example2 class

---
+++ Example2.cs
@@ -0,0 +1,13 @@
+using System;
+using static System.Console;
+
+namespace Capgemini.CSharp.Day2
+{
+    public class Example2
+    {static void Main(string[] args)
+    {
+        
+   
+        WriteLine("Hello world");
+    }
+}}
